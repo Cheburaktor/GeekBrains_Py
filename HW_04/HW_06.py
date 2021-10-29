cf799a45333f2f649b27c2b5cf9d093b69e74d91
Cheburaktor <92298785+Cheburaktor@users.noreply.github.com>
2021-10-30 00:58:52 +0300

    Update HW_06.py

--- HW_04/HW_06.py
+++ HW_04/HW_06.py
@@ -2,13 +2,28 @@
 # а) итератор, генерирующий целые числа, начиная с указанного,
 # б) итератор, повторяющий элементы некоторого списка, определенного заранее.
 
-from itertools import count
+from itertools import count, cycle
 
-for el in count(int(input('Введите стартовое число '))):
-    print(el) # внимание - беконечный цикл!
+#Var1
 
-from itertools import cycle
+print("*" * 20, "Var1")
+start_iterator = 7
 
-my_list = [True, 'ABC', 123, None]
-for el in cycle(my_list):
-    print(el) # внимание - беконечный цикл!
+for el in count(start_iterator):
+    if el > 10:
+        break
+
+    print(el)
+
+# var2
+print("*" * 20, "Var2")
+cycling_list = [2, 5, 6, 8, 32, 140]
+max_iteration = 12
+iteration_count = 0
+
+for el in cycle(cycling_list):
+    print(el)
+    iteration_count += 1
+    
+    if iteration_count >= max_iteration:
+        break
