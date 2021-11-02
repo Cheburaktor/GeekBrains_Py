311f2ad42e32d240035a704dda74a8f043e59ad4
Cheburaktor <92298785+Cheburaktor@users.noreply.github.com>
2021-11-02 16:00:15 +0300

    Update HW05_05.py

--- HW_05/HW05_05.py
+++ HW_05/HW05_05.py
@@ -1,2 +1,14 @@
 """Создать (программно) текстовый файл, записать в него программно набор чисел,
 разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран."""
+
+from random import randrange
+
+rand_num = [randrange(1, 200) for _ in range(50)]
+
+with open("num_02", "w") as output:
+    output.write(" ".join((map(str, rand_num))))
+
+with open ("num_02") as inp:
+    numbers = inp.read().split()
+    
+    print(sum(int(x) for x in numbers))
