6ccfe807576bd8f3d44bdf417a71e384a1883408
Cheburaktor <92298785+Cheburaktor@users.noreply.github.com>
2021-11-02 16:05:07 +0300

    Update HW05_06.py

--- HW_05/HW05_06.py
+++ HW_05/HW05_06.py
@@ -3,3 +3,25 @@
 предмету и их количество. Важно, чтобы для каждого предмета не обязательно были все типы занятий.
 Сформировать словарь, содержащий название предмета и общее количество занятий по нему.
 Вывести словарь на экран."""
+
+
+sub = {}
+
+with open("lessons.txt") as file:
+    for row in file:
+        sub_info = row.split()
+        name = sub_info[0].rstrip(":")
+        sub[name] = sub_info[1:]
+
+result = {}
+
+for key, value in sub.items():
+    result[key] = sum(
+        [
+            int(hours[:hours.index("(")])
+            for hours in value
+            if hours != "-"
+        ]
+    )
+
+print(result)
