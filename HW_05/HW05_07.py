f228ba9771cfd8659381d8270bcb458c518de8c8
Cheburaktor <92298785+Cheburaktor@users.noreply.github.com>
2021-11-02 16:11:45 +0300

    Update HW05_07.py

--- HW_05/HW05_07.py
+++ HW_05/HW05_07.py
@@ -8,3 +8,28 @@
 (со значением убытков).
 Пример списка: [{“firm_1”: 5000, “firm_2”: 3000, “firm_3”: 1000}, {“average_profit”: 2000}].
 Итоговый список сохранить в виде json-объекта в соответствующий файл."""
+
+import json
+
+firms_data = []
+
+with open("firms.txt") as input_data:
+    firm_dict = {}
+    profit_list = []
+
+    for firm_row in input_data:
+        name, form, revenue, costs = firm_row.split()
+
+        profit = float(revenue) - float(costs)
+        firm_dict[name] = profit
+
+        if profit:
+            profit_list.append(profit)
+
+    firms_data.append(firm_dict)
+    firms_data.append({
+        "avg_profit": round(sum(profit_list) / len(profit_list), 2)
+    })
+
+with open("firms.txt", "w") as output_data:
+    json.dump(firms_data, output_data)
