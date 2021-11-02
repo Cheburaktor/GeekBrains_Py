3d2244350f2c9610a2e047963ce2f9c5a59da9a4
Cheburaktor <92298785+Cheburaktor@users.noreply.github.com>
2021-11-02 15:56:03 +0300

    Update HW05_04.py

--- HW_05/HW05_04.py
+++ HW_05/HW05_04.py
@@ -2,3 +2,21 @@
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл."""
 
+trans = {
+    'One': 'Один'
+    'Two': 'Два'
+    'Three': 'Три'
+    'Four' 'Четыре'
+
+}
+
+conv = []
+
+with open('numbers') as data:
+    for row in data:
+        name, value = row.split(' - ')
+        conv.append(f"{trans[name]} - {value}")
+        
+with open ("numbers", "w") as out_data:
+    out_data.writelines(conv)
+
