ead819d89dee0587f29e22956ff92f7e46d4d863
Cheburaktor <92298785+Cheburaktor@users.noreply.github.com>
2021-11-12 09:41:56 +0300

    Update HW01_01.py

--- HW_07/HW01_01.py
+++ HW_07/HW01_01.py
@@ -5,36 +5,44 @@
 класса Matrix (двух матриц). Результатом сложения должна быть новая матрица."""
 
 class Matrix:
-    def __init__(self, list_1, list_2):
-        # self.matr = [list_1, list_2]
-        self.list_1 = list_1
-        self.list_2 = list_2
+    value: list = []
 
-    def __add__(self):
-        matr = [[0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0]]
+    def __init__(self, value: list):
+        self.value = value
 
-        for i in range(len(self.list_1)):
+    def __add__(self, other: 'Matrix'):
+        try:
+            rows_count = len(self.value)
+            items_count = len(self.value[0])
 
-            for j in range(len(self.list_2[i])):
-                matr[i][j] = self.list_1[i][j] + self.list_2[i][j]
+            new_value = [
+                [
+                    self.value[row][idx] + other.value[row][idx]
+                    for idx in range(items_count)
+                ]
+                for row in range(rows_count)
+            ]
 
-        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
-
+            return Matrix(new_value)
+        except IndexError:
+            print("Матрицы разного размера")
+            exit(1)
 
     def __str__(self):
-        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
+        return "\n".join(
+            str(row).strip('[]'). replace(',', '')
+            for row in self.value
+        )
 
+a = Matrix([
+    [15, 16, 18],
+    [19, 15, 16],
+])
 
+b = Matrix([
+    [20, 21, 22],
+    [26, 27, 28],
+])
 
-my_matrix = Matrix([[5, 18, 11],
-                    [6, 17, 23],
-                    [41, 50, 9]],
-                   [[45, 8, 2],
-                    [6, 7, 93],
-                    [24, 5, 97]])
-# result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-
-print(my_matrix.__add__())
+c = a + b
+print(c)
